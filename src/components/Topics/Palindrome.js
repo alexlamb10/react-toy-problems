1529246cdab7a1140f1e9133cf6b420c54a69475
Alex Lamb <alexlamb104@gmail.com>
2022-04-12 00:30:52 -0600

    Added functionality to check for Palindrome

--- src/components/Topics/Palindrome.js
+++ src/components/Topics/Palindrome.js
@@ -1,9 +1,45 @@
 import React, { Component } from 'react'
 
 export default class Palindrome extends Component {
+
+  constructor() {
+    super()
+    this.state={
+      userInput: '',
+      palindrome: ''
+    }
+  }
+
+  checkPalindrome(prop) {
+    const stringArray = prop.split('')
+    const reverseString = stringArray.reverse().join('')
+
+    if(prop === reverseString){
+      this.setState({
+        palindrome: 'True'
+      })
+  }else{
+    this.setState({
+      palindrome: 'False'
+    })
+  }
+
+}
+
   render() {
     return (
-      <div>Palindrome</div>
+      <div className="puzzleBox palindromePB">
+              <h4>Palindrome</h4>
+              <input type="text" className='inputLine' onChange={(e) => {
+                this.setState({
+                  userInput: e.target.value
+                })
+              }}/>
+              <button className="confirmationButton" onClick={() => {
+                this.checkPalindrome(this.state.userInput)
+              }}>Submit</button>
+              <span className="resultsBox">Palindrome? {this.state.palindrome}</span>
+          </div>
     )
   }
 }
